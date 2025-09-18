b3d5a1b5f2d10745e3369c1cacfccdbc63949a4b
Benjamin Meredith <bmer@udel.edu>
2025-09-18 18:40:25 -0400

    Completed function 5 of task 6

--- src/objects.ts
+++ src/objects.ts
@@ -80,7 +80,12 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let result = `# ${question.name}\n${question.body}`;
+    if (question.type === "multiple_choice_question") {
+        const options = question.options.map(opt => `- ${opt}`).join("\n");
+        result += `\n${options}`;
+    }
+    return result;
 }
 
 /**
