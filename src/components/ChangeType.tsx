2911af0b6436bbdbe2577b168b38a1a99f832dde
Benjamin Meredith <bmer@udel.edu>
2025-09-25 21:42:40 -0400

    Completed ChangeType.tsx

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,18 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function changeType(): void {
+        setType((current) =>
+            current === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    return (
+    <div>
+      <div>Question Type: {type}</div>
+      <Button onClick={changeType}>Change Type</Button>
+    </div>
+  );
 }
