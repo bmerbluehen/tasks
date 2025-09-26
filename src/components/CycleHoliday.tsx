96da578f1edaaa077f50506397a2d053ab62da25
Benjamin Meredith <bmer@udel.edu>
2025-09-25 23:13:01 -0400

    Completed CycleHoliday.tsx

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,33 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday = "🍺" | "😴" | "🏅" | "💘" | "🎄"; //National Beer Day, World Sleep Day, National Sports Day, Valentine's Day, Christmas
+
+const nextHoliday: Record<Holiday, Holiday> = {
+    "💘": "😴",
+    "😴": "🍺",
+    "🍺": "🏅",
+    "🏅": "🎄",
+    "🎄": "💘",
+};
+
+const nextByAlphabet: Record<Holiday, Holiday> = {
+    "🎄":"🍺",
+    "🍺":"🏅",
+    "🏅":"💘",
+    "💘":"😴",
+    "😴":"🎄",
+};
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("🍺");
+
+    return (
+        <div>
+        <div>Holiday: {holiday}</div>
+        <Button onClick={() => {setHoliday(nextByAlphabet[holiday])}}>Advanced by Alphabet</Button>
+        <Button onClick={() => {setHoliday(nextHoliday[holiday])}}>Advanced by Year</Button>
+
+        </div>
+    );
 }
