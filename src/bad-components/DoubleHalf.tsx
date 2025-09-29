794a3d970f839f1ba27bfac885de26dbd57365f9
Benjamin Meredith <bmer@udel.edu>
2025-09-28 21:34:06 -0400

    Fixed DoubleHalf.tsx and deleted DoubleHalfState.tsx

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,40 +1,36 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
+interface DoubleHalfProps {
+    clicked: () => void;
 }
 
-function Halver(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
+function Doubler({ clicked }: DoubleHalfProps): React.JSX.Element {
+    return <Button onClick={clicked}>Double</Button>;
+}
+
+function Halver({ clicked }: DoubleHalfProps): React.JSX.Element {
+    return <Button onClick={clicked}>Halve</Button>;
 }
 
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+
+    const double = () => {
+        setDhValue(dhValue * 2);
+    };
+    const half = () => {
+        setDhValue(dhValue / 2);
+    };
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler clicked={double}></Doubler>
+            <Halver clicked={half}></Halver>
         </div>
     );
 }
