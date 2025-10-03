f5298f390ceb3e28f4acc2ac7c22c42857095fa2
Benjamin Meredith <bmer@udel.edu>
2025-10-02 21:45:14 -0400

    Completed MultipleChoiceQuestion.tsx

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+    const isCorrect = choice === expectedAnswer;
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <select
+                value={choice}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                    setChoice(e.target.value);
+                }}
+            >
+                {options.map((opt) => (
+                    <option key={opt} value={opt}>
+                        {opt}
+                    </option>
+                ))}
+            </select>
+
+            <span>{isCorrect ? "✔️" : "❌"}</span>
         </div>
     );
 }
